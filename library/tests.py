b8e40c6cfbc867827656d6856ec84fc7e83fa045
dika <chieduchineme@gmail.com>
2025-08-15 14:18:12 +0200

    test.py

--- library/tests.py
+++ library/tests.py
@@ -10,7 +10,33 @@ class LibraryTrackingTests(TestCase):
         self.client = APIClient
         self.author = Author.objects.create(first_name="dika", last_name="Chiedu")
         self.member = User.objects.create(user_name="Chiedu")
-        self.book = Book.objects.create(first_name="dika", last_name="Chiedu")
-        # self.author = Author.objects.create(first_name="dika", last_name="Chiedu")
+        self.book = Book.objects.create(title="Test Book", author=self.author, isbn='fake_isbn')
+        self.member = Member.objects.create(user=self.member)
+
+        self.overdue_loan = Loan.objects.create(
+            book=self.book,
+            member=self.member,
+            due_date=timezone.now() - timezone.timedelta(days=1),
+            is_returned=False,
+        )
+        self.returned_loan = Loan.objects.create(
+            book=self.book,
+            member=self.member,
+            due_date=timezone.now() - timezone.timedelta(days=1),
+            is_returned=True,
+        )
+        self.active_loan = Loan.objects.create(
+            book=self.book,
+            member=self.member,
+            due_date=timezone.now() + timezone.timedelta(days=1),
+            is_returned=False,
+        )
+
+    @patch("library.tasks.send_mail")
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+    def test_check_overdue_loans(self, send_mail_mock):
+        check_overdue_loans.delay()
+
+        self.assertTrue(send_mail_mock.called)
 
 # Create your tests here.
